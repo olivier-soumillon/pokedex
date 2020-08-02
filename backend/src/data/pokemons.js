52748282df6b25fa1c726ef9fc357c1b28e6b1ee
Olivier Soumillon <olivier.soumillon@gmail.com>
2020-08-02 16:21:05 +0200

    set up fake database

---
+++ backend/src/data/pokemons.js
@@ -0,0 +1,53 @@
+export default [{
+    id: 'pokemon-1',
+    name: 'Pikachu',
+    types: [
+        'electric'
+    ]
+}, {
+    id: 'pokemon-2',
+    name: 'Squirtle',
+    types: [
+        'water'
+    ]
+}, {
+    id: 'pokemon-3',
+    name: 'Charmander',
+    types: [
+        'fire'
+    ]
+}, {
+    id: 'pokemon-4',
+    name: 'Bulbasaur',
+    types: [
+        'grass'
+    ]
+}, {
+    id: 'pokemon-5',
+    name: 'Staryu',
+    types: [
+        'water',
+        'psychic'
+    ]
+}, {
+    id: 'pokemon-6',
+    name: 'Starmie',
+    types: [
+        'water',
+        'psychic'
+    ]
+}, {
+    id: 'pokemon-7',
+    name: 'Onix',
+    types: [
+        'rock',
+        'ground'
+    ]
+}, {
+    id: 'pokemon-8',
+    name: 'Geodude',
+    types: [
+        'rock',
+        'ground'
+    ]
+}]
---
+++ backend/src/data/trainers.js
@@ -0,0 +1,36 @@
+export default [{
+    id: 'trainer-1',
+    fullName: 'Ash Ketchum',
+    team: [
+        'pokemon-1',
+        'pokemon-2',
+        'pokemon-3',
+        'pokemon-4'
+    ],
+    friends: [
+        'trainer-2',
+        'trainer-3'
+    ]
+}, {
+    id: 'trainer-2',
+    fullName: 'Misty Williams',
+    team: [
+        'pokemon-5',
+        'pokemon-6'
+    ],
+    friends: [
+        'trainer-1',
+        'trainer-3'
+    ]
+}, {
+    id: 'trainer-3',
+    fullName: 'Brock',
+    team: [
+        'pokemon-7',
+        'pokemon-8'
+    ],
+    friends: [
+        'trainer-1',
+        'trainer-2'
+    ]
+}]
